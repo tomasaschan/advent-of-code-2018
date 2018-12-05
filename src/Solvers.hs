525756f0995f11ec3d39bdca19b2482796ed76ce
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2018-12-05 09:48:59 +0100

    Solve Dec 5 (with _very_ suboptimal runtime on b)

--- src/Solvers.hs
+++ src/Solvers.hs
@@ -5,6 +5,7 @@ import qualified Aoc2017.Dec1
 import qualified Dec1
 import qualified Dec2
 import qualified Dec3
+import qualified Dec5
 
 solver :: String -> Maybe ([String] -> String)
 solver "2017-1a" = Just Aoc2017.Dec1.solveA
@@ -15,4 +16,6 @@ solver "2a" = Just Dec2.solveA
 solver "2b" = Just Dec2.solveB
 solver "3a" = Just Dec3.solveA
 solver "3b" = Just Dec3.solveB
+solver "5a" = Just Dec5.solveA
+solver "5b" = Just Dec5.solveB
 solver _ = Nothing
