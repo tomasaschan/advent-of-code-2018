140476cc0bca36e45da59f33e5a7db345ece16a0
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2018-12-07 16:40:07 +0100

    Solve Dec 6

--- src/Solvers.hs
+++ src/Solvers.hs
@@ -1,11 +1,11 @@
 module Solvers where
 
-import Text.Printf
 import qualified Aoc2017.Dec1
 import qualified Dec1
 import qualified Dec2
 import qualified Dec3
 import qualified Dec5
+import qualified Dec6
 
 solver :: String -> Maybe ([String] -> String)
 solver "2017-1a" = Just Aoc2017.Dec1.solveA
@@ -18,4 +18,6 @@ solver "3a" = Just Dec3.solveA
 solver "3b" = Just Dec3.solveB
 solver "5a" = Just Dec5.solveA
 solver "5b" = Just Dec5.solveB
+solver "6a" = Just Dec6.solveA
+solver "6b" = Just $ Dec6.solveB 10000
 solver _ = Nothing
