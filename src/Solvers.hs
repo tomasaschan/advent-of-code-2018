63c691a7738fe61d7b6d6c9fe2d546c07053e61b
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2018-12-18 21:40:34 +0100

    Solve Dec 11

--- src/Solvers.hs
+++ src/Solvers.hs
@@ -8,6 +8,7 @@ import qualified Dec4
 import qualified Dec5
 import qualified Dec6
 import qualified Dec8
+import qualified Dec11
 
 solver :: String -> Maybe ([String] -> String)
 solver "2017-1a" = Just Aoc2017.Dec1.solveA
@@ -26,4 +27,6 @@ solver "6a" = Just Dec6.solveA
 solver "6b" = Just $ Dec6.solveB 10000
 solver "8a" = Just Dec8.solveA
 solver "8b" = Just Dec8.solveB
+solver "11a" = Just Dec11.solveA
+solver "11b" = Just Dec11.solveB
 solver _ = Nothing
