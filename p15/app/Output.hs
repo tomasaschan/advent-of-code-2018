5d59791b5924decc7ed15b81d63739a4c1f2d670
tomasaschan <1550920+tomasaschan@users.noreply.github.com>
2019-07-04 14:15:47 +0200

    Port dec 13-15

--- p15/app/Output.hs
+++ p15/app/Output.hs
@@ -1,18 +1,18 @@
 module Output where
 
-import Solvers.Dec15
-import Data.Function (on)
-import Data.List (sortBy, groupBy, intercalate)
-import Data.Map (toList)
-import Data.Tuple (swap)
-import System.Console.ANSI
-import Text.Printf
+import           Data.Function       (on)
+import           Data.List           (groupBy, intercalate, sortBy)
+import           Data.Map            (toList)
+import           Data.Tuple          (swap)
+import           Solver
+import           System.Console.ANSI
+import           Text.Printf
 
 setColor :: ColorIntensity -> Color -> IO ()
 setColor i c = setSGR [SetColor Foreground i c]
 
 goTo :: Int -> Int -> IO ()
-goTo x y = setCursorPosition (y+2) x
+goTo x y = setCursorPosition (y + 2) x
 
 drawMap :: Dungeon -> IO ()
 drawMap dungeon = do
@@ -23,8 +23,9 @@ drawMap dungeon = do
     --   setColor Dull White
     --   goTo x y
     --   putChar '#'
-    drawTerrain (_, Wall) = do return ()
-    drawTerrain ((x,y), Cavern) = do
+    drawTerrain (_, Wall) = do
+      return ()
+    drawTerrain ((x, y), Cavern) = do
       setColor Dull White
       goTo x y
       putChar '.'
@@ -41,7 +42,7 @@ defaultColor (Unit Elf _ _ _)    = Red
 defaultColor (Unit Goblin _ _ _) = Green
 
 drawUnit :: Color -> Unit -> IO ()
-drawUnit clr (Unit r (x,y) _ _) = do
+drawUnit clr (Unit r (x, y) _ _) = do
   setColor Vivid clr
   goTo x y
   putChar $ defaultChar r
@@ -55,13 +56,13 @@ markUnit u = do
 
 showState :: Int -> Dungeon -> [Unit] -> IO ()
 showState r dungeon units = do
-  let (x,y) = size dungeon
+  let (x, y) = size dungeon
   clearScreen
   putStrLn ""
   showRoundNumber r
   drawMap dungeon
   drawUnits units
-  drawHitPoints (x+3) units
+  drawHitPoints (x + 3) units
   goTo 0 (y + 1)
   putStrLn ""
 
@@ -75,7 +76,10 @@ showRoundNumber r = do
 
 drawHitPoints :: Int -> [Unit] -> IO ()
 drawHitPoints offset units = do
-  let sorted = groupBy ((==) `on` (snd . position)) . sortBy (compare `on` (swap . position)) $ units
+  let sorted =
+        groupBy ((==) `on` (snd . position)) .
+        sortBy (compare `on` (swap . position)) $
+        units
   setColor Dull White
   sequence_ . fmap drawLineHp $ sorted
   where
@@ -83,12 +87,12 @@ drawHitPoints offset units = do
       let y = snd . position . head $ row
       goTo (offset + 1) y
       putStr . intercalate ", " . fmap showUnitHp $ row
-    showUnitHp (Unit Elf _ _ hp) = printf "E(%d)" hp
+    showUnitHp (Unit Elf _ _ hp)    = printf "E(%d)" hp
     showUnitHp (Unit Goblin _ _ hp) = printf "G(%d)" hp
 
 endCombat :: Dungeon -> IO ()
 endCombat dungeon = do
-  let (_,y) = size dungeon
+  let (_, y) = size dungeon
   setColor Vivid White
-  goTo 0 (y+1)
+  goTo 0 (y + 1)
   putStrLn ""
---
+++ p15/test/SolverSpec.hs
@@ -0,0 +1,270 @@
+module SolverSpec where
+
+import           Data.Pathfinding
+import           Solver
+import           Test.Hspec
+
+import           Data.Map         (fromList)
+import           GHC.Exts         (the)
+
+spec :: Spec
+spec =
+  describe "Dec 15" $ do
+    context "parsing" $ do
+      let input = ["#######", "#.G.E.#", "#E.G.E#", "#.G.E##", "#######"]
+      let units =
+            [ Unit Goblin (2, 1) 3 200
+            , Unit Elf (4, 1) 3 200
+            , Unit Elf (1, 2) 3 200
+            , Unit Goblin (3, 2) 3 200
+            , Unit Elf (5, 2) 3 200
+            , Unit Goblin (2, 3) 3 200
+            , Unit Elf (4, 3) 3 200
+            ]
+      let dungeon = fromList [((x, y), wall x y) | x <- xs, y <- ys]
+            where
+              xs = [0 .. 6]
+              ys = [0 .. 4]
+              wall x y
+                | x == 0 || x == 6 || y == 0 || y == 4 || (x, y) == (5, 3) =
+                  Wall
+                | otherwise = Cavern
+      let realInput =
+            [ "################################"
+            , "##########..####################"
+            , "##########..G###################"
+            , "##########..#.....########.#####"
+            , "##########........########G#####"
+            , "############...#..########.#####"
+            , "################....######.#####"
+            , "#################..G####...#####"
+            , "################...#..#....#####"
+            , "################...G..#.....E###"
+            , "##############.G..........G....#"
+            , "###########.G...G..............#"
+            , "###########G..#####..........###"
+            , "###########..#######.........###"
+            , "##########.G#########........#.#"
+            , "#########...#########....G.....#"
+            , "#########...#########.........##"
+            , "##..........#########.........##"
+            , "######....G.#########.....E....#"
+            , "##...........#######.......#...#"
+            , "#...G.........#####E.......#####"
+            , "##....................#..#######"
+            , "##.G.................##.########"
+            , "##..#GG.............###...#..###"
+            , "#G..#..G.G........G.####.#..E###"
+            , "#.....#.##...........###.....###"
+            , "#######...............###EE..###"
+            , "########.....E........###....###"
+            , "########..............####..####"
+            , "##########....E....#...###.#####"
+            , "###########...EE....#.##########"
+            , "################################"
+            ]
+      it "parses units correctly" $ do parseUnits 3 input `shouldBe` units
+      it "parses map correctly" $ do parseDungeon input `shouldBe` dungeon
+    context "walkability" $ do
+      let dungeon =
+            fromList
+              [ ((0, 0), Wall)
+              , ((1, 0), Wall)
+              , ((2, 0), Wall)
+              , ((3, 0), Wall)
+              , ((4, 0), Wall)
+              , ((5, 0), Wall)
+              , ((0, 1), Wall)
+              , ((1, 1), Cavern)
+              , ((2, 1), Cavern)
+              , ((3, 1), Cavern)
+              , ((4, 1), Wall)
+              , ((5, 1), Wall)
+              , ((0, 2), Wall)
+              , ((1, 2), Wall)
+              , ((2, 2), Cavern)
+              , ((3, 2), Cavern)
+              , ((4, 2), Cavern)
+              , ((5, 2), Wall)
+              , ((0, 3), Wall)
+              , ((1, 3), Wall)
+              , ((2, 3), Wall)
+              , ((3, 3), Cavern)
+              , ((4, 3), Cavern)
+              , ((5, 3), Wall)
+              , ((0, 4), Wall)
+              , ((1, 4), Cavern)
+              , ((2, 4), Cavern)
+              , ((3, 4), Wall)
+              , ((4, 4), Cavern)
+              , ((5, 4), Wall)
+              , ((0, 5), Wall)
+              , ((1, 5), Wall)
+              , ((2, 5), Wall)
+              , ((3, 5), Wall)
+              , ((4, 5), Wall)
+              , ((5, 5), Wall)
+              ]
+      let units = [Unit Elf (1, 1) 3 200, Unit Goblin (4, 3) 3 200]
+      it "does not allow walking into walls" $ do
+        walkable dungeon units (1, 2) `shouldBe` False
+      it "does not allow walking outside of the map" $ do
+        walkable dungeon units (6, 4) `shouldBe` False
+      it "does not allow walking into other units" $ do
+        walkable dungeon units (4, 3) `shouldBe` False
+      it "allows walking into unoccupied open spaces" $ do
+        walkable dungeon units (2, 1) `shouldBe` True
+    it "can sort units according to reading order" $ do
+      let unsorted =
+            [ Unit Elf (4, 3) 3 200
+            , Unit Elf (4, 1) 3 200
+            , Unit Elf (1, 2) 3 200
+            , Unit Elf (5, 2) 3 200
+            , Unit Goblin (2, 1) 3 200
+            , Unit Goblin (3, 2) 3 200
+            , Unit Goblin (2, 3) 3 200
+            ]
+      let sorted =
+            [ Unit Goblin (2, 1) 3 200
+            , Unit Elf (4, 1) 3 200
+            , Unit Elf (1, 2) 3 200
+            , Unit Goblin (3, 2) 3 200
+            , Unit Elf (5, 2) 3 200
+            , Unit Goblin (2, 3) 3 200
+            , Unit Elf (4, 3) 3 200
+            ]
+      inReadingOrder unsorted `shouldBe` sorted
+    describe "examples for a" $ do
+      it "full example" $ do
+        let input =
+              [ "#######"
+              , "#.G...#"
+              , "#...EG#"
+              , "#.#.#G#"
+              , "#..G#E#"
+              , "#.....#"
+              , "#######"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (47, 590, 27730, 0)
+      it "first example" $ do
+        let input =
+              [ "#######"
+              , "#G..#E#"
+              , "#E#E.E#"
+              , "#G.##.#"
+              , "#...#E#"
+              , "#...E.#"
+              , "#######"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (37, 982, 36334, 5)
+      it "second example" $ do
+        let input =
+              [ "#######"
+              , "#E..EG#"
+              , "#.#G.E#"
+              , "#E.##E#"
+              , "#G..#.#"
+              , "#..E#.#"
+              , "#######"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (46, 859, 39514, 5)
+      it "third example" $ do
+        let input =
+              [ "#######"
+              , "#E.G#.#"
+              , "#.#G..#"
+              , "#G.#.G#"
+              , "#G..#.#"
+              , "#...E.#"
+              , "#######"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (35, 793, 27755, 0)
+      it "fourth example" $ do
+        let input =
+              [ "#######"
+              , "#.E...#"
+              , "#.#..G#"
+              , "#.###.#"
+              , "#E#G#G#"
+              , "#...#G#"
+              , "#######"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (54, 536, 28944, 0)
+      it "fifth example" $ do
+        let input =
+              [ "#########"
+              , "#G......#"
+              , "#.E.#...#"
+              , "#..##..G#"
+              , "#...##..#"
+              , "#...#...#"
+              , "#.G...G.#"
+              , "#.....G.#"
+              , "#########"
+              ]
+        let (dungeon, units) = initial 3 input
+        let answer = summarize . play dungeon $ units
+        answer `shouldBe` (20, 937, 18740, 0)
+    context "pathfinding" $ do
+      it "chooses the correct target square" $ do
+        let world = ["#######", "#E..G.#", "#...#.#", "#.G.#G#", "#######"]
+        let (dungeon, units) = initial 3 world
+        let targets =
+              foldMap (inRangeOf . position) . filter ((==) Goblin . race) $
+              units
+        let (Just path) = shortestPathReadingOrder dungeon units (1, 1) targets
+        last path `shouldBe` (3, 1)
+      it "chooses the correct square to move to" $ do
+        let (dungeon, _) =
+              initial 3 ["#######", "#.....#", "#.....#", "#.....#", "#######"]
+        let mover = Unit Elf (2, 1) 0 0
+        let rest = [Unit Goblin (4, 3) 0 0]
+        let targets = inRangeOf (4, 3)
+        let moved = move dungeon rest targets mover
+        position moved `shouldBe` (3, 1)
+      it "finds cadaker's subtle bug" $ do
+        let (dungeon, units) =
+              initial
+                3
+                [ "#######"
+                , "#..E..#"
+                , "#.###.#"
+                , "#.#...#"
+                , "#...#.#"
+                , "###G###"
+                , "#######"
+                ]
+        let mover = the . filter ((==) Elf . race) $ units
+        let target = the . filter ((==) Goblin . race) $ units
+        let targets = inRangeOf . position $ target
+        let moved = move dungeon units targets mover
+        position moved `shouldBe` (2, 1)
+      it "chooses targets in reading order" $ do
+        let (dungeon, units) =
+              initial
+                3
+                [ "######"
+                , "#E...#"
+                , "#.##.#"
+                , "#..#.#"
+                , "##.#.#"
+                , "#..#.#"
+                , "#.##.#"
+                , "#..G.#"
+                , "######"
+                ]
+        let mover = the . filter ((==) Elf . race) $ units
+        let target = the . filter ((==) Goblin . race) $ units
+        let targets = inRangeOf . position $ target
+        let moved = move dungeon units targets mover
+        position moved `shouldBe` (1, 2)
