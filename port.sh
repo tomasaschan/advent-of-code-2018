f223432cc4a75cba0b5a5dda9d8f4feb3b166c43
tomasaschan <1550920+tomasaschan@users.noreply.github.com>
2019-07-04 10:36:45 +0000

    Tweak port script

--- port.sh
+++ port.sh
@@ -4,12 +4,9 @@ PROBLEM=$1
 
 ./new.sh $PROBLEM
 
-cp ./src/Solvers/Dec${PROBLEM/#0}.hs p$PROBLEM/lib/Solver.hs
-cp ./test/Solvers/Dec${PROBLEM/#0}Spec.hs p$PROBLEM/test/Tests.hs
-
 sed -i "s/Dec PROBLEM/Dec ${PROBLEM/#0}/" p$PROBLEM/app/Main.hs
-sed -i "s/Solvers.Dec${PROBLEM/#0}/Solver/" p$PROBLEM/lib/Solver.hs
-sed -i "s/Solvers.Dec${PROBLEM/#0}Spec/Tests/" p$PROBLEM/test/Tests.hs
-sed -i "s/Solvers.Dec${PROBLEM/#0}/Solver/" p$PROBLEM/test/Tests.hs
+sed -e "s/Solvers.Dec${PROBLEM/#0}/Solver/" ./src/Solvers/Dec${PROBLEM/#0}.hs > p$PROBLEM/lib/Solver.hs
+sed -e "s/Solvers.Dec${PROBLEM/#0}/Solver/" -e "s/Solvers.Dec${PROBLEM/#0}Spec/Tests/" ./test/Solvers/Dec${PROBLEM/#0}Spec.hs > p$PROBLEM/test/SolverSpec.hs
 
-stylish-haskell ./p$PROBLEM/lib/Solver.hs ./p$PROBLEM/test/Tests.hs
+hindent ./p$PROBLEM/lib/Solver.hs ./p$PROBLEM/test/SolverSpec.hs
+stylish-haskell -i ./p$PROBLEM/lib/Solver.hs ./p$PROBLEM/test/SolverSpec.hs
