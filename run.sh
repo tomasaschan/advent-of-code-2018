8f0dfde4183f2e05802044819f8769e01fa524b9
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2021-01-01 00:23:43 +0000

    Tweak project setup

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 PROGRAM=$1
 shift
-stack build p$PROGRAM $@ --exec "bash -c \"p$PROGRAM < ./input/dec${PROGRAM/#0}.txt\""
+if test -n "${1-}"; then
+    LABEL="-$1"
+    shift
+else
+    LABEL=""
+fi
+stack build p$PROGRAM $@ --exec "bash -c \"p$PROGRAM < ./input/dec${PROGRAM/#0}$LABEL.txt\""
