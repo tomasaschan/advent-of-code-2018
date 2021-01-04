c54f514236192a3c453b9f1c77538faf02440809
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2021-01-04 11:47:43 +0000

    Ensure new project is runnable

--- new.sh
+++ new.sh
@@ -4,3 +4,4 @@ PROBLEM=$1
 cp -R template "solvers/p$PROBLEM"
 sed -i "s/PROBLEM/p$PROBLEM/g" solvers/"p$PROBLEM"/package.yaml
 sed -i "s%# PROBLEM%- solvers/p$PROBLEM\n  # PROBLEM%" stack.yaml
+sed -i "s/PROGLEM/$PROBLEM" solvers/p"$PROBLEM"/app/Main.hs
