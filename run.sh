2a6ae96a4e0f77005935d1182965e7362173d111
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2021-01-30 17:32:24 +0000

    Accept non-label flags and pass them to Stack

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 PROGRAM=$1
 shift
-if test -n "${1-}"; then
+if test -n "${1-}" && ! [[ ${1-} = -* ]]; then
     LABEL="-$1"
     shift
 else
     LABEL=""
 fi
 set -x
-stack build utils p$PROGRAM $@ --test --exec "bash -c \"p$PROGRAM < ./input/dec${PROGRAM/#0}$LABEL.txt\""
+stack build utils p$PROGRAM "$@" --test --exec "bash -c \"p$PROGRAM < ./input/dec${PROGRAM/#0}$LABEL.txt\""
